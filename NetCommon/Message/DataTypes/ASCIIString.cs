6b4867f384836c4cfe201bb4f45b4e26356a2ab0
Neriya Cohen <ee10293847@gmail.com>
2021-01-27 02:27:15 +0200

    Created String class for sending over the network with GameCore.Net

---
+++ NetCommon/Message/DataTypes/ASCIIString.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+namespace GameCore.Net
+{
+    public class ASCIIString : StringBase
+    {
+        public ASCIIString() : base(Encoding.ASCII) { }
+
+        public ASCIIString(string str) : base(str, Encoding.ASCII) { }
+    }
+}
---
+++ NetCommon/Message/DataTypes/UTF16String.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+namespace GameCore.Net
+{
+    public class UTF16String :  StringBase
+    {
+        public UTF16String() : base(Encoding.Unicode) { }
+
+        public UTF16String(string str) : base(str, Encoding.Unicode) { }
+    }
+}
